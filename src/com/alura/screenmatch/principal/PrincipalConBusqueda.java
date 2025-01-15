d83050814e50e14a7398f74f8757eaf7374d8d7d
Gustavo Aguilar <gaguilarf@unsa.edu.pe>
2025-01-15 00:06:59 -0500

    Final del curso

---
+++ src/com/alura/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,78 @@
+package com.alura.screenmatch.principal;
+
+import com.alura.screenmatch.excepcion.ErrorEnConversionDeDuracionException;
+import com.alura.screenmatch.modelos.Titulo;
+import com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner lectura = new Scanner(System.in);
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        while (true) {
+            System.out.println("Escriba el nombre de una pelicula: ");
+            var busqueda = lectura.nextLine();
+
+            if (busqueda.equalsIgnoreCase("salir")){
+                break;
+            }
+            String direccion = "https://www.omdbapi.com/?t="+busqueda.replace(" ","+")+"&apikey=d4d0bf92";
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(miTituloOmdb);
+
+                Titulo miTitulo = new Titulo(miTituloOmdb);
+                System.out.println(miTitulo);
+
+                titulos.add(miTitulo);
+                FileWriter escritura = new FileWriter("titulos.json");
+                escritura.write(gson.toJson(titulos));
+                escritura.close();
+
+            } catch (NumberFormatException e){
+                System.out.println("Ocurrio un error");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error en la uri, verifique la dirección");
+                System.out.println(e.getMessage());
+            } catch (ErrorEnConversionDeDuracionException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error inesperado");
+            }
+        }
+
+        System.out.println(titulos);
+
+    }
+}
